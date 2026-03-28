04cfcfdc30005ac4552b886222c53f0a0dfdd9d8
amat3029-dot <amat3029@gmail.com>
2026-03-28 15:09:57 +0530

    Fixed routing using HashRouter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Navbar from './components/Navbar';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from './components/Home'
 import About from './components/About'
 import NoteState from './context/notes/NoteState';
@@ -8,13 +8,15 @@ import Alert from './components/Alert';
 import Login from './components/Login';
 import Signup from './components/Signup';
 import { useState } from 'react';
+
 function App() {
   const [alert, setAlert] = useState();
+
   const showAlert = (message, type) => {
     setAlert({
       message: message,
       type: type
-    })
+    });
     setTimeout(() => {
       setAlert(null);
     }, 2000);
@@ -22,18 +24,18 @@ function App() {
 
   return (
     <NoteState>
-      <BrowserRouter>
+      <HashRouter>
         <Navbar />
-      <Alert alert={alert}/>
+        <Alert alert={alert}/>
         <div className="container">
           <Routes>
-            <Route path="/"  element={<Home showAlert={showAlert}/>} />
+            <Route path="/" element={<Home showAlert={showAlert}/>} />
             <Route path="/about" element={<About />} />
-            <Route path="/login"  element={<Login  showAlert={showAlert}/>} />
-            <Route path="/signup" element={<Signup  showAlert={showAlert}/>} />
+            <Route path="/login" element={<Login showAlert={showAlert}/>} />
+            <Route path="/signup" element={<Signup showAlert={showAlert}/>} />
           </Routes>
         </div>
-      </BrowserRouter>
+      </HashRouter>
     </NoteState>
   );
 }
